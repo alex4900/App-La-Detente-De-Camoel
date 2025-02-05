af360046964ec57499c243e762bb7d061d0a8dcc
Alexandre Allain <alexandre.allain495@gmail.com>
2025-02-05 12:21:55 +0100

    amélioration du homepagescreen

--- lib/ui/screens/HomePageScreen.dart
+++ lib/ui/screens/HomePageScreen.dart
@@ -85,4 +85,17 @@ class _HomePageScreenState extends State<HomePageScreen> {
       _currentIndex = index + 1;
     });
   }
+
+  PageRouteBuilder customPageRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(position: animation.drive(tween), child: child);
+      },
+    );
+  }
 }
