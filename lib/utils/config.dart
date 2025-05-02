62833a8a8b58c6948427ea3a4f9884b5cbaf98af
Alexandre Allain <alexandre.allain495@gmail.com>
2025-05-02 13:57:00 +0200

    apk fonctionnel

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,4 +1,8 @@
 class AppConfig {
+  // URL de l'API utilisée en mode debug et en production
   static const String baseUrl = 'http://ladetentedecamoel-web-7uucjr-41b780-192-168-157-50.traefik.me/api';
   static const String ordersEndpoint = '$baseUrl/addCommande';
+  
+  // Configuration pour autoriser le trafic non sécurisé (HTTP)
+  static const bool allowInsecureConnections = true;
 }
