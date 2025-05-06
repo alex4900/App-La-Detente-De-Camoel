e75d68ed9db8a0d356fdeb2bd05756e3828b4247
Alexandre Allain <alexandre.allain495@gmail.com>
2025-05-06 11:12:32 +0200

    config modif

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // URL de l'API utilisée en mode debug et en production
-  static const String baseUrl = 'http://ladetentedecamoel-web-7uucjr-41b780-192-168-157-50.traefik.me/api';
+  static const String baseUrl = 'http://examen-apimobile-marqau-8dc3f4-192-168-136-2.traefik.me/api';
   static const String ordersEndpoint = '$baseUrl/addCommande';
   
   // Configuration pour autoriser le trafic non sécurisé (HTTP)
